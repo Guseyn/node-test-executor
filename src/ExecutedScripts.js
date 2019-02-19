ef69d8aae18173ec32ae26fc88228fc0ef41af90
guseyn <guseyn@guseyns-MacBook-Pro.local>
2019-02-19 22:14:19 +0400

    #71

--- src/ExecutedScripts.js
+++ src/ExecutedScripts.js
@@ -4,20 +4,40 @@ const AsyncObject = require('@cuties/cutie').AsyncObject
 const ExecutedCommand = require('./ExecutedCommand')
 const FinalMessage = require('./FinalMessage')
 
+class CallbackForFinalMessage extends AsyncObject {
+  constructor (callback, finalMessage) {
+    super(callback, finalMessage)
+  }
+
+  definedSyncCall () {
+    return (callback, finalMessage) => {
+      callback(finalMessage)
+      return callback
+    }
+  }
+}
+
+// Represented result is final message
 class ExecutedScripts extends AsyncObject {
   constructor (scripts, executedTime) {
     super(scripts, executedTime)
   }
 
-  definedSyncCall () {
-    return (scripts, executedTime) => {
+  definedAsyncCall () {
+    return (scripts, executedTime, callback) => {
       let commands = []
       scripts.forEach(script => {
         commands.push(new ExecutedCommand(script, executedTime))
       })
-      new FinalMessage(executedTime, ...commands).call()
-      return scripts
+      new CallbackForFinalMessage(
+        callback,
+        new FinalMessage(executedTime, ...commands)
+      ).call()
     }
+  }
+
+  callbackWithError () {
+    return false
   }
 }
 
